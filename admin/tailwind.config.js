8999008ff55ea1aec7a3ae3ea05286b613201df4
Rumesh Siriwardhana <it21388934@my.sliit.lk>
2024-11-28 22:29:09 +0530

    initiat project

---
+++ admin/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FFFFFF",
+        secondary: {
+          100: "#523B2F",
+          200: "#EBE2DB",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
